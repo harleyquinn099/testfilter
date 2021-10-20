5eb98b13e1fd76a744c80b876af57e2d6f829c77
HARLEY QUINN <88760900+harleyquinn099@users.noreply.github.com>
2021-10-20 12:12:48 +0530

    Update translation.py

--- bot.py/translation.py
+++ bot.py/translation.py
@@ -1,63 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG @MRK_YT
+# (c) @AlbertEinsteinTG
 
 class Translation(object):
     
-    START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
-
-<b>I am a Pro AutoFilterBot V2....😜</b>
-
-<b>Make me an admin for your group and channel then connect me....🎉</b>
-
-<b>🙂Watch the video below to build an Pro Autofilter bot V2 like me...</b>
-
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
-
-<b>Press /help to know about available commands🤪</b>"""    
+    START_TEXT = """<b>Hey😁 {}!!</b>
+ഞാനൊരു Advance AutofilterBot ആണ്😉
+പക്ഷെ വെറുതെ Start ഞെക്കി സമയം കളയേണ്ട😁 എന്നെ HECTOR ന് മാത്രമേ ഉപയോഗിക്കാൻ സാധിക്കൂ... 🤷‍♀️
+"""    
     
     HELP_TEXT = """
-<b>😉How To Use Me!😉?</b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - <b>To disconnect A Group With A Channel</b>
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
-    
-    -> <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-
-            -> <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
-            
-            -> <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
-
-            -> <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
-            
-            -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
-            
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+ഹയ്യട😝! നീയെന്തിനാ Help Button ഞെക്കിയത്🤷‍♀️.വെറുതെ ഇതിൽ ഞെക്കിക്കൊണ്ടിരുന്നിട്ട് ഒരു കാര്യവുമില്ല😁.അവൻ Help ചോദിച്ച് വന്നേക്കുന്നു🤦‍♀️.ഇപ്പൊ ചെയ്യും Help നോക്കിയിരുന്നോ...😂
+<b><i><a href="https://t.me/Unlimtedmovie00">FILM GALLERY</a></i></b>
 """
     
-    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
-
-<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/AlbertEinstein_TG">👤Albert Einstein</a></b>
-
-<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
-
-<b>➥ 🗣️Language</b> : <b>Python3<b>
-
-<b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
-
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
-"""
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Advance Auto Filter Bot
